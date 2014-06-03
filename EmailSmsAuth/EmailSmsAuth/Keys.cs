67ec5ce09e33d218802a8a5bdb17c5c680ab80f7
Johnny Iverson Kikambi <xplicit4548@gmail.com>
2014-06-03 12:15:28 +0200

    Add EmailSmsAuth

---
+++ EmailSmsAuth/EmailSmsAuth/Keys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmailSmsAuth
+{
+    public static class Keys
+    {
+
+        /*
+         * AccountSid and AuthToken from Twilio api
+         */
+        public static string TwilioSid = "AC8a2df98537ad29712c43d0afed30ceba";
+        public static string TwilioToken = "0b785db1ff4feababa667fb9c620eda7";
+
+        /*
+         * Sandbox phone number for trial accounts or an SMS-enabled Twilio phone number you purchased for upgraded accounts.
+        */
+        public static string FromPhone = "+15592016138";
+
+    }
+}
